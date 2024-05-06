31b721673d31b50e0055e44aed1b98c0e5a430b3
yuyangzhang <84109471+YuyangisCoding@users.noreply.github.com>
2024-05-06 17:34:04 +1000

    update

--- link_prediction/params.py
+++ link_prediction/params.py
@@ -5,7 +5,7 @@ def parse_args():
     parser.add_argument('--lr', default=1e-4, type=float, help='learning rate')
     parser.add_argument('--batch', default=20480, type=int, help='training batch size')
     parser.add_argument('--tst_batch', default=256, type=int, help='testing batch size (number of users)')
-    parser.add_argument('--epoch', default=100, type=int, help='number of epochs')
+    parser.add_argument('--epoch', default=50, type=int, help='number of epochs')
     parser.add_argument('--save_path', default='tem', help='file name to save model and training record')
     parser.add_argument('--load_model', default=None, help='model name to load')
     parser.add_argument('--data', default='', type=str, help='name of dataset')
